0f9b3079dfbb12e23a22807932f52252ad2ba384
MarcusVinix <vi.ni159marcus@gmail.com>
2022-06-25 15:01:45 -0300

    arrumando caminho da imagem

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import imgCodeMockupUrl from '../../src/assets/code-mockup.png'
 
 export function Subscribe() {
 
@@ -70,7 +71,7 @@ export function Subscribe() {
 				</div>
 			</div>
 
-			<img src="/src/assets/code-mockup.png" alt="" className="mt-10"/>
+			<img src={imgCodeMockupUrl} alt="" className="mt-10"/>
 		</div>
 	)
 }
